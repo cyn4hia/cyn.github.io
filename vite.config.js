15f1f27d3eb7ae57fd777922a0b8446e22976f78
cyn <letcindyknow@gmail.com>
2026-03-05 16:18:19 -0500

    my code is doomed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Change '/green-grapes/' to match your GitHub repo name
   base: "/cyn4hia.github.io/",
 });
